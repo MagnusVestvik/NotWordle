a80f435b8e65774542b29c41292b5d29125c2a45
MagnusVestvik <magnusvestvik@proton.me>
2024-10-01 20:03:17 +0200

    feat update terminalColors with the color red
    there was a issue where it was a bit hard to see if you gave an invalid
    input, this have now been fixed by changing the color of invalid input
    strings to red.

--- Wordle/src/main/java/org/sportradar/util/TerminalColors.java
+++ Wordle/src/main/java/org/sportradar/util/TerminalColors.java
@@ -2,10 +2,10 @@ package org.sportradar.util;
 
 public class TerminalColors {
 
-    // Color codes
     private static final String YELLOW = "\u001B[33m";
     private static final String GREEN = "\u001B[32m";
     private static final String RESET = "\u001B[0m";
+    private static final String RED = "\u001B[31m";
 
     /**
      * @param text to color
@@ -23,4 +23,12 @@ public class TerminalColors {
         return YELLOW + text + RESET;
     }
 
+
+    /**
+     * @param text to color
+     * @return the text in red.
+     */
+    public static String red(String text) {
+        return RED + text + RESET;
+    }
 }
